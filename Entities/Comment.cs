e67a0023ef1a3db2cacdf8229a12969981dbedad
BaghirzadeMamedik <brizeforge@gmail.com>
2022-03-12 11:12:17 +0400

    Add project files.

---
+++ Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Comment : Base
+    {
+        public string CommentText { get; set; }
+        public string K205userId { get; set; }
+        public K205User K205User { get; set; }
+    }
+}
